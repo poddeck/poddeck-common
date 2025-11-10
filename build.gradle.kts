e9ceab9b150efe94399eb4c2bf4b9d2088ac0065
Lukas <contact@lukasbreuer.de>
2025-11-10 14:30:04 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+  id("java")
+  id("maven-publish")
+  id("io.freefair.lombok") version "9.1.0"
+}
+
+group = "io.poddeck"
+version = "1.0.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_25
+java.targetCompatibility = JavaVersion.VERSION_25
+
+publishing {
+  publications {
+    register<MavenPublication>("gpr") {
+      from(components["java"])
+    }
+  }
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/poddeck/poddeck-common")
+      credentials {
+        username = System.getenv("GITHUB_USERNAME") ?: findProperty("github.username") as String?
+        password = System.getenv("GITHUB_TOKEN") ?: findProperty("github.token") as String?
+      }
+    }
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation(platform("org.junit:junit-bom:6.0.1"))
+  testImplementation("org.junit.jupiter:junit-jupiter:6.0.1")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.1")
+
+  implementation("com.google.inject:guice:7.0.0")
+
+  implementation("com.google.guava:guava:33.5.0-jre")
+
+  implementation("org.projectlombok:lombok:1.18.42")
+  annotationProcessor("org.projectlombok:lombok:1.18.42")
+  testImplementation("org.projectlombok:lombok:1.18.42")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
+
+  implementation("org.json:json:20250517")
+  implementation("commons-io:commons-io:2.20.0")
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
